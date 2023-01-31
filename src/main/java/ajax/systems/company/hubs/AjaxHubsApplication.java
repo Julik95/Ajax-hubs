137ae159ba00d3c9ebfda16a9f78086df48ecf90
Yuliy Khlyebnikov <y.khlyebnikov@reply.it>
2023-01-31 13:57:14 +0100

    Inizializzato il WS

---
+++ src/main/java/ajax/systems/company/hubs/AjaxHubsApplication.java
@@ -0,0 +1,13 @@
+package ajax.systems.company.hubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxHubsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjaxHubsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ajax/systems/company/hubs/AjaxHubsApplicationTests.java
@@ -0,0 +1,13 @@
+package ajax.systems.company.hubs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxHubsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
