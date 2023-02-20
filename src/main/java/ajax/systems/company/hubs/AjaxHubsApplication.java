3386e253d543dd0ab397d03f3b865b981b051d8a
Yuliy Khlyebnikov <y.khlyebnikov@reply.it>
2023-02-20 07:44:43 +0100

    Aggiornato gerarchia della cartella root del progetto

--- src/main/java/ajax/systems/company/hubs/AjaxHubsApplication.java
+++ src/main/java/ajax/systems/company/hubs/AjaxHubsApplication.java
@@ -1,6 +1,9 @@
 package ajax.systems.company.hubs;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +18,6 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 @SpringBootApplication
@@ -33,9 +34,9 @@ public class AjaxHubsApplication extends Application{
 	@Override
 	public void start(Stage stage) throws Exception {
 		if(root != null) {
-	        JFXDecorator decorator = new JFXDecorator(stage, root);
+	        JFXDecorator decorator = new JFXDecorator(stage, root, false, true, true);
 	        decorator.setCustomMaximize(true);
-	        decorator.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+	        decorator.getStylesheets().addAll(loadStyles());
 	        Scene scene = new Scene(decorator);
 			stage.setScene(scene);
 			stage.setTitle("Ajax security system");
@@ -63,6 +64,18 @@ public class AjaxHubsApplication extends Application{
 			logger.error("Error occured during credentials fxml parsing", e);
 			return null;
 		}
+	}
+	
+	private List<String> loadStyles() {
+		ArrayList<String> result = new ArrayList();
+		File cssDir = new File("css");
+		for (File css : cssDir.listFiles()) {
+			if(css.isFile()) {
+				logger.info("Load style {}", css.getAbsolutePath());
+			}
+			result.add("file:///" + css.getAbsolutePath().replace("\\", "/"));
+		}
+		return result;
 	}
 
 }
